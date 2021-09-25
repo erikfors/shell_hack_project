598bfc83276a944b3ebfc3c2de0dcd8351310146
erikfors <efors003@fiu.edu>
2021-09-25 19:44:51 -0400

    Not Final

--- lib/app/providers/coinroutes.dart
+++ lib/app/providers/coinroutes.dart
@@ -15,7 +15,7 @@ class CoinRoutesProvider {
 
   final _channel = WebSocketChannel.connect(
     Uri.parse(
-        'wss://staging.coinroutes.com/api/streaming/cbbo/?token=6c634e1eacecc4801b000249287fbf923d5c8824'),
+        'wss://staging.coinroutes.com/api/streaming/real_price/?token=6c634e1eacecc4801b000249287fbf923d5c8824'),
   );
 
   Future<List<CurrencyPair>> getCurrencyPairs() async {
@@ -40,7 +40,7 @@ class CoinRoutesProvider {
       ),
       data: {
         "currency_pair": pair,
-        "exchanges": ["gdax", "gemini", "bitstamp"],
+        "exchanges": ["gdax", "gemini", "bitstamp","kraken"],
         "side": "bids",
         "quantity": quantity,
         "use_fees": true,
